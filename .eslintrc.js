fcdd4bb14c3bd4c466419b63c0b096b8377d11aa
siyeon <sylee@megazone.com>
2021-09-11 02:04:53 +0900

    feat: add package, fix config for handling lint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,12 @@ module.exports = {
         "plugin:vue/vue3-strongly-recommended", // for vue3
         "plugin:@typescript-eslint/recommended",
         "eslint:recommended",
-        "@vue/typescript/recommended"
+        "@vue/typescript/recommended",
     ],
-    "parser": "vue-eslint-parser",
+    parser: "vue-eslint-parser",
     parserOptions: {
         ecmaVersion: 12,
-        parser: '@typescript-eslint/parser',
+        parser: "@typescript-eslint/parser",
         sourceType: 'module',
         "vueFeatures": {
             "filter": true,
@@ -29,25 +29,23 @@ module.exports = {
     rules: {
         indent: ["error", 4],
         "vue/component-name-in-template-casing": ["error", "kebab-case"],
-        "max-len": ["error", { code: 200 }],
         "vue/script-setup-uses-vars": 'error',
         "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        "vue/html-indent": ["error", 4],
         "prefer-destructuring": ["error", { object: false, array: false }],
         radix: ["error", "as-needed"],
         "no-prototype-builtins": "error",
         "no-empty": ["error", { allowEmptyCatch: true }],
-        "import/extensions": [
-            "error",
-            "always",
-            {
-                js: "never",
-                jsx: "never",
-                ts: "never",
-                tsx: "never"
-            }
-        ],
+        // "import/extensions": [
+        //     "error",
+        //     "always",
+        //     {
+        //         js: "never",
+        //         jsx: "never",
+        //         ts: "never",
+        //         tsx: "never"
+        //     }
+        // ],
         camelcase: [
             "error",
             { properties: "always", allow: ["translation_id", "domain_id", "item_type", "region_id", "collector_id", "plugin_info", "plugin_id"] }
@@ -67,13 +65,14 @@ module.exports = {
         "no-empty-function": ["error", { allow: ["constructors"] }],
         "no-param-reassign": ["error", { props: false }],
         "no-underscore-dangle": ["off"],
-        "import/no-extraneous-dependencies": [
-            "error",
-            {
-                devDependencies: true
-            }
-        ],
+        // "import/no-extraneous-dependencies": [
+        //     "error",
+        //     {
+        //         devDependencies: true
+        //     }
+        // ],
         "import/prefer-default-export": ["off"],
+        "import/no-unresolved": "off",
         "no-new": ["off"],
         "prefer-template": ["error"],
         "no-plusplus": ["off"],
@@ -110,7 +109,7 @@ module.exports = {
     },
     settings: {
         "import/resolver": {
-            typescript: {}
+            typescript: { }
         }
     }
 };
