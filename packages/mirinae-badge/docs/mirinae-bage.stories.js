f28cf3d8bbe5c843acad669d9eb401ec83e4679d
siyeon <sylee@megazone.com>
2021-09-29 17:02:51 +0900

    feat: storybook setting

---
+++ packages/mirinae-badge/docs/mirinae-bage.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// We want to always get from source
+import { Badge } from '../lib/mirinae-badge';
+
+export default { title: 'Badge' };
+
+export const small = () => <Badge>Hello Badge</Badge>;
